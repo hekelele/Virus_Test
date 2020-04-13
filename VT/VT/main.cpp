407949f900f36c8e0b08bccb390a7d0079657c90
kabucha <gavin-li@outlook.com>
2020-04-14 01:03:33 +0800

    10个一组测试

--- VT/VT/main.cpp
+++ VT/VT/main.cpp
@@ -6,14 +6,46 @@ int* solve(TestAgent* agent)
 {
 	int length = agent->getSampleNumber();
 	int* result = new int[length];
-	int* one = new int[1];
-	for(int i = 0; i < length; i++) {
-		one[0] = i;
-		result[i] = agent->test(one, 1);
+	int start = 0;
+	int end = start + 10;
+	int* tests = new int[10];
+	int exitFlag = 0;
+	while (1) {
+		if (end>=length) {
+			exitFlag = 1;
+			end = length;
+		}
+
+		int len = end - start;
+		for (int i = 0; i < len; i++) {
+			tests[i] = start + i;
+		}
+
+		int temp = agent->test(tests,len);
+
+		if (temp == 0) {
+			for (int i = 0; i < len; i++) {
+				result[start + i] = 0;
+			}
+		}
+		else
+		{
+			for (int i = 0; i < len; i++) {
+				tests[0] = start + i;
+				result[start + i] = agent->test(tests,1);
+			}
+		}
+
+		if (exitFlag) {
+			break;
+		}
+		start += 10;
+		end += 10;
 	}
 	
 	return result;
 }
+
 
 int main()
 {
